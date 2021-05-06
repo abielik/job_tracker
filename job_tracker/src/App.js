e6bbbe4b2d60512feec611123dd57c699d2e59ea
Alan Bielik <abielik92@gmail.com>
2021-05-06 17:23:16 -0400

    Added Navbar. No functionality

--- job_tracker/src/App.js
+++ job_tracker/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 import JobBoard from './components/JobBoard';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <JobBoard />
     </div>
   );
