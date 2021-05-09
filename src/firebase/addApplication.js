ae1dc571f9751e4106b6f37701e551154b480128
Alan Bielik <abielik92@gmail.com>
2021-05-09 18:19:13 -0400

    added addApplication.js. Need to test functionality

---
+++ src/firebase/addApplication.js
@@ -0,0 +1,28 @@
+import firebase from './index';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+export async function addApplication(
+  userId,
+  title,
+  company,
+  dateApplied,
+  status
+) {
+  try {
+    const applicationsCollectionRef = firestore
+      .collection('users')
+      .doc(userId)
+      .collection('Job Applications');
+
+    await applicationsCollectionRef.add({
+      title,
+      company,
+      dateApplied,
+      status,
+    });
+  } catch (err) {
+    console.log('Origin: addApplication.addApplication(): ', err);
+  }
+}
