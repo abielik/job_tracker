f020892f6bf36eee755dd2fbd71195762a471150
Alan Bielik <abielik92@gmail.com>
2021-05-09 19:13:28 -0400

    added AddApplicationForm to test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+//import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+
+import AddApplicationForm from './AddApplicationForm';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,6 +18,7 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+    margin: theme.spacing(1),
   },
 }));
 
@@ -34,9 +37,16 @@ function Navbar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant='h6' className={classes.title}>
-            Home
-          </Typography>
+
+          <Button
+            className={classes.title}
+            variant='contained'
+            color='inherit'
+            fullWidth='false'
+          >
+            Add Application
+          </Button>
+
           <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
